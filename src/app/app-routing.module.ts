bbf0bb6503138ed613cebbb694ed9512297308e6
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-04-12 23:38:03 -0300

    configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListClassesComponent} from "./pages/ebd/list-classes/list-classes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"classes",component:ListClassesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
