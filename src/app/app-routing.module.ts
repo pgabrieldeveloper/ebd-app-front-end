62bf007c329476c6a859d4ae0ef465a11d073e44
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-04-13 07:01:15 -0300

    feat list-aulas => adicionando pagina de listar aula e adicionando ao path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListClassesComponent} from "./pages/ebd/list-classes/list-classes.component";
+import {ListAulasComponent} from "./pages/ebd/list-aulas/list-aulas.component";
 
 const routes: Routes = [
-  {path:"classes",component:ListClassesComponent}
+  {path:"classes",component:ListClassesComponent},
+  {path:"aulas",component:ListAulasComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListClassesComponent } from './pages/ebd/list-classes/list-classes.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { ListAulasComponent } from './pages/ebd/list-aulas/list-aulas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListClassesComponent,
-    HeaderComponent
+    HeaderComponent,
+    ListAulasComponent
   ],
   imports: [
     BrowserModule,
